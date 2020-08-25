55f19d4dd4400623b8048d5974526c53b3c9f5e4
emajidev <emajidev@gmail.com>
2020-08-24 20:06:33 -0400

    Updates

---
+++ precache-manifest.e9f3910b7c37e48c18193b135e36f3a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04706f04a0099f51be0f9618c86fe1b3",
+    "url": "/webportfolio/index.html"
+  },
+  {
+    "revision": "6c9ad4ad84c58407bf84",
+    "url": "/webportfolio/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ee2ca3b85bd95893e498",
+    "url": "/webportfolio/static/css/main.69d8635a.chunk.css"
+  },
+  {
+    "revision": "6c9ad4ad84c58407bf84",
+    "url": "/webportfolio/static/js/2.b40704f8.chunk.js"
+  },
+  {
+    "revision": "0a083a6215e9f980f5787fed849f9591",
+    "url": "/webportfolio/static/js/2.b40704f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2ca3b85bd95893e498",
+    "url": "/webportfolio/static/js/main.fd27268c.chunk.js"
+  },
+  {
+    "revision": "cb9436a75f227da2f46e",
+    "url": "/webportfolio/static/js/runtime-main.a3056c69.js"
+  }
+]);
